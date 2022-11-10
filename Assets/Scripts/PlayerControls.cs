3dd510c78bd628dc7093fac5d42cdeb42043ec17
tiger <thisisstreamliner@gmail.com>
2022-11-11 00:15:17 +0200

    Add Header and Tooltips Attributes for PlayerControls.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
-{
+{   
+    [Header("Input Controls")]
     [SerializeField] InputAction movement;
     [SerializeField] InputAction fireAction;
-    [SerializeField] float controlSpeed = 30f;
-    [SerializeField] float xRange = 6f;
-    [SerializeField] float yRange = 4f;
 
+    [Header("General Setup Settings")]
+    [Tooltip("How fast ship moves up and down based upon player input")][SerializeField] float controlSpeed = 30f;
+    [Tooltip("How far player moves horizontally")][SerializeField] float xRange = 6f;
+    [Tooltip("How far player moves vertically")][SerializeField] float yRange = 4f;
+
+    [Header("Laser gun array")]
+    [Tooltip("Add all player laser here")]
     [SerializeField] ParticleSystem[] lasers;
 
+    [Header("Screen position based tuning")]
     [SerializeField] float positionPitchFactor = -2f;
-    [SerializeField] float controlPitchFactor = -15f;
     [SerializeField] float positionYawFactor = 2f;
+
+    [Header("Player input based tuning")]
+    [SerializeField] float controlPitchFactor = -15f;
     [SerializeField] float controlRollFactor = -20f;
     private float _horizontalThrow;
     private float _verticalThrow;
