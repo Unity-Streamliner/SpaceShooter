659e432497a58a4a41fb3b4188dc436fa4754d17
tiger <thisisstreamliner@gmail.com>
2022-11-14 23:54:28 +0200

    Reload Scene After Collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
     void OnCollisionEnter(Collision other)
     {
         Debug.Log(this.name + "--Collided with--" + other.gameObject.name);
+        
     }
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+        if (other.gameObject.tag == "Enemy") 
+        {
+            StartCrushSequence();
+        }
+    }
+
+    private void StartCrushSequence()
+    {
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
